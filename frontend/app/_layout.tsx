4d61dad156e1d2181d0d908d0a40d662428b2fb0
Azure愛 <kenieldeguzman45@gmail.com>
2026-04-01 21:55:22 +0800

    Update _layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import "../global.css";
 import * as SystemUI from 'expo-system-ui';
+import { ImageBackground } from 'react-native'; 
 
 SystemUI.setBackgroundColorAsync('#3723A9');
 
@@ -15,6 +16,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={DefaultTheme}>
+       <ImageBackground 
+        source={require('../assets/images/background-image.jpg')}
+        style={{ flex: 1 }}
+        imageStyle={{ opacity: 0.2}}
+      ></ImageBackground>
       <Stack initialRouteName='(tabs)'
              screenOptions={{
               animation: 'slide_from_bottom',
