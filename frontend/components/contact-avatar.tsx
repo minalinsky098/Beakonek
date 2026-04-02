8dc59c89e91bf8a8ff4d635b69f9626121cabac2
Sol-19 <joshabrahampanoy@gmail.com>
2026-04-02 12:42:02 +0800

    added logs.json

--- frontend/components/contact-avatar.tsx
+++ frontend/components/contact-avatar.tsx
@@ -6,9 +6,15 @@ import contacts from '@/data/contacts.json';
 export default function ContactAvatar ()
 
 {
-  const formatNumber = (number: string) => 
-        {const digits = number.replace('639', '');
-        return `+639 ${digits.slice(0, 3)} ${digits.slice(3, 6)} ${digits.slice(6)}`;};
+    const colors = ['#3723A9', '#FF6B2C', '#E91E63', '#009688', '#FF5722', '#673AB7', '#2196F3', '#4CAF50'];
+    const formatNumber = (number: string) => 
+            {const digits = number.replace('639', '');
+            return `+639 ${digits.slice(0, 3)} ${digits.slice(3, 6)} ${digits.slice(6)}`;};
+    const getColor = (name: string) => {
+    const index = name.charCodeAt(0) % colors.length;
+    return colors[index];
+    };
+    
         
                 
                                 
@@ -20,7 +26,8 @@ export default function ContactAvatar ()
                     {contacts.map((contact) => (
                       <View key={contact.relative_id} className="items-center mr-6">
 
-                        <View className="w-16 h-16 rounded-full bg-[#3723A9] justify-center items-center mb-2">
+                        <View style={{ backgroundColor: getColor(contact.relative_name) }}
+                        className="w-16 h-16 rounded-full justify-center items-center mb-2">
                           <Text className="text-white text-xl font-bold">
                             {contact.relative_name.charAt(0).toUpperCase()}
                           </Text>
